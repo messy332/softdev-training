392b0596b704535d35a50de1bcfb8416c01599f4
messy332 <t96016304@gmail.com>
2025-07-18 10:54:20 +0800

    Update LoginView.vue: correct branding spelling from 'SofTrain' to 'SoftTrain'

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -47,7 +47,7 @@ const visible = ref(false)
                     position-fixed
                     opacity-20
                   >
-                    <h2 class="d-flex justify-center mb-2">SofTrain</h2>
+                    <h2 class="d-flex justify-center mb-2">SoftTrain</h2>
 
                     <div class="mx-auto" max-width="344">
                       <v-text-field
